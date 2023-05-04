4a9be5d1f3fb9076d96e17e7a0f675c817855d1c
Zigatase <zigatase@gmail.com>
2023-05-04 21:30:44 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Include.h
@@ -0,0 +1,9 @@
+#ifndef  INCLUDE_H
+#define INCLUDE_H
+
+// Includ's
+
+#include <iostream>
+#include <Windows.h>
+
+#endif // INCLUDE_H
---
+++ TestTask/TestTask.cpp
@@ -0,0 +1,147 @@
+﻿#include "Include.h"
+
+using namespace std;
+
+//
+class GetLocalDrive
+{
+private:
+    char LogicalDisks[512];
+    char LogicalDisksName[512];
+
+public:
+    void test()
+    {
+        //
+        DWORD LogicalDisksLen{ GetLogicalDriveStringsA(sizeof(LogicalDisks), LogicalDisks) };
+
+        //
+        BOOL test = GetVolumeInformationA(
+            "P:\\",
+            LogicalDisksName,
+            sizeof(LogicalDisksName),
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            0
+        );
+
+
+        DWORD i{ 0 };
+        do
+        {
+            if (i == 0)
+                cout << LogicalDisks << endl;
+
+            if (LogicalDisks[i] == 0)
+            {
+                cout << LogicalDisks + i + 1 << endl;
+            }
+
+            i += 1;
+        } while (i < LogicalDisksLen - 2);
+    }
+
+};
+
+//
+void EnumeratingLocalDisks();
+void EnumerationsOfAllFiles();
+void CollectingInformation();
+
+//
+int main()
+{
+    CollectingInformation();
+
+    return 0;
+}
+
+
+// Part #1
+void EnumeratingLocalDisks()
+{
+    //
+    char LogicalDisks[512];
+    char LogicalDisksName[512];
+    char SystemNet[512];
+
+    //
+    DWORD Num;
+
+    //
+    unsigned long long lpTotalNumberOfBytes;
+    unsigned long long lpTotalNumberOfFreeBytes;
+
+
+    //
+    DWORD LogicalDisksLen{ GetLogicalDriveStringsA(sizeof(LogicalDisks), LogicalDisks) };
+
+
+    DWORD i{ 0 };
+    do
+    {
+        if (i == 0)
+        {
+            // Get Name Toms and File System Tom
+            BOOL GetVoluInfoDisk = GetVolumeInformationA(
+                LogicalDisks,
+                LogicalDisksName,
+                sizeof(LogicalDisksName),
+                &Num,
+                NULL,
+                NULL,
+                SystemNet,
+                sizeof(SystemNet)
+            );
+
+            cout << LogicalDisks << LogicalDisksName << endl;
+        }
+
+        if (LogicalDisks[i] == 0)
+        {
+            // Getting Name Toms and File System Tom
+            BOOL GetVoluInfoDisk = GetVolumeInformationA(
+                LogicalDisks + i + 1,
+                LogicalDisksName,
+                sizeof(LogicalDisksName),
+                &Num,
+                NULL,
+                NULL,
+                SystemNet,
+                sizeof(SystemNet)
+            );
+
+            // Getting the volume size in bytes
+            BOOL Get = GetDiskFreeSpaceExA(
+                LogicalDisks,
+                NULL,
+                (PULARGE_INTEGER)&lpTotalNumberOfBytes,
+                (PULARGE_INTEGER)&lpTotalNumberOfFreeBytes
+            );
+
+            cout << LogicalDisks + i + 1 << LogicalDisksName << "\t" << SystemNet << "\t" << lpTotalNumberOfBytes << endl;
+        }
+
+        i += 1;
+    } while (i < LogicalDisksLen - 2);
+}
+
+
+// Part #2
+void EnumerationsOfAllFiles()
+{
+
+}
+
+
+// Part #3
+void CollectingInformation()
+{
+    char buffer[256];
+    DWORD size = 256;
+    GetUserNameA(buffer, &size);
+
+    cout << buffer << endl;
+}
